0cd1a1941993a44e004fe006b3e46ea2a670cb0d
Sasha Koshka <supergriffin64@gmail.com>
2021-05-18 18:08:04 -0400

    Made a better way to load images

--- main.c
+++ main.c
@@ -24,6 +24,7 @@ SDL_Event     event;
 SDL_Texture  *framesTex[TEXLEN];
 
 void frame(int);
+char* catstr(char*, char*);
 Uint32 idleTick(Uint32, void*);
 
 int main(int argc, char *argv[]) {
@@ -58,6 +59,54 @@ int main(int argc, char *argv[]) {
   
   // Load assets
   
+  char *genericResPath = "img/";
+  char *unixResPath    = "/usr/share/icons/desktop-raine/";
+  char *resPath;
+  char iconPath[128]   = {0};
+  
+  if(argc > 1) {
+    resPath = argv[1];
+  } else if(access(genericResPath, F_OK) == 0) {
+    resPath = genericResPath;
+  } else if(access(unixResPath, F_OK) == 0) {
+    resPath = genericResPath;
+  } else {
+    SDL_ShowSimpleMessageBox(
+      SDL_MESSAGEBOX_ERROR,
+      "Could not get images",
+      "Could not get images. Make sure program is installed.",
+      window
+    );
+    goto error;
+  }
+  
+  catstr(catstr(iconPath, resPath), "icon.png");
+  SDL_Surface *icon = IMG_Load(iconPath);
+  SDL_SetWindowIcon(window, icon);
+  
+  char *framesName[TEXLEN] = {
+    "blank",
+    "idle_1",
+    "idle_2",
+    "blink"
+  };
+  
+  char resPaths[TEXLEN][128] = {0};
+  for(int i = 0; i < TEXLEN; i++) {
+    catstr(
+      catstr(
+        catstr(
+          resPaths[i],
+          resPath),
+        framesName[i]),
+      ".png"
+    );
+    printf("%s\n", resPaths[i]);
+  }
+  
+  SDL_Surface *framesImg[TEXLEN];
+  
+  /*
   char *unixResPath[TEXLEN] = {
     "/usr/share/icons/desktop-raine/blank.png",
     "/usr/share/icons/desktop-raine/idle_1.png",
@@ -72,8 +121,6 @@ int main(int argc, char *argv[]) {
     "img/blink.png",
   };
   
-  SDL_Surface *icon;
-  SDL_Surface *framesImg[TEXLEN];
   
   if(access(genericResPath[0], F_OK) == 0) {
     icon = IMG_Load("img/icon.png");
@@ -92,13 +139,14 @@ int main(int argc, char *argv[]) {
     );
     goto error;
   }
+  */
   
-  SDL_SetWindowIcon(window, icon);
-  
-  for(int i = 0; i < TEXLEN; i++)
+  for(int i = 0; i < TEXLEN; i++) {
+    framesImg[i] = IMG_Load(resPaths[i]);
     framesTex[i] = SDL_CreateTextureFromSurface(
       renderer, framesImg[i]
     );
+  }
   
   // Loop
   
@@ -169,4 +217,19 @@ Uint32 idleTick(Uint32 interval, void *param) {
 
   SDL_PushEvent(&event);
   return(interval);
+}
+
+/*
+  catstr
+  Adds the second char* to the first one, starting at wherever
+  the first pointer points. Returns the pointer to the index it
+  left off on so it can be daisy chained.
+*/
+char* catstr(char *paper, char *pen) {
+  while(*pen) {
+    *paper = *pen;
+    paper++;
+    pen++;
+  }
+  return paper;
 }
