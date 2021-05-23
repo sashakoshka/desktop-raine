c3ab2a908ebe6e555ae418c9620f9d3dcc30c242
Sasha Koshka <supergriffin64@gmail.com>
2021-05-23 09:44:09 -0400

    Removed SDL2 image from windown build script

--- win-build.bat
+++ win-build.bat
@@ -1,1 +1,1 @@
-gcc main.c -mwindows -ISDL2-win\include -LSDL2-win\lib -Wall -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -o raine.exe
+gcc main.c -mwindows -ISDL2-win\include -LSDL2-win\lib -Wall -lmingw32 -lSDL2main -lSDL2 -o raine.exe
